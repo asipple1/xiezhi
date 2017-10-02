612bafd0cf1a32fe257e9274b56e6493c83006a2
Andy Sipple <Andy>
2017-10-01 21:57:25 -0500

    Add color to backgound

--- src/components/Gify.vue
+++ src/components/Gify.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="gify">
     <form class="" v-on:submit.prevent="fetchData">
-      <input type="text" class="search" v-model="searchKey"/>
+      <input type="text" class="search" v-model="searchKey" placeholder="Search Something Funny!" autofocus/>
     </form>
-    <div class='grid' v-for='gif in gify'>
-      <img class='grid-item' v-bind:src="gif.images.original.url">
+    <div class='grid'>
+      <img v-for='gif in gify' class='grid-item' v-bind:src="gif.images.original.url">
     </div>
   </div>
 </template>
@@ -12,7 +12,7 @@
 <script>
   import axios from 'axios'
   const gifyApiUrl = 'http://api.giphy.com/v1/gifs/search?q='
-  const gifyApiKey = '&api_key=S2yXMMc8VI8qHZNdR2PWH086Ff8fI52w&limit=5'
+  const gifyApiKey = '&api_key=S2yXMMc8VI8qHZNdR2PWH086Ff8fI52w&limit=16'
 
   export default {
     name: 'Gify',
@@ -39,3 +39,42 @@
     }
   }
 </script>
+
+<style lang="scss">
+
+$colors: red, orange, yellow, green, blue, purple;
+$repeat: 20;
+
+@for $i from 1 through $repeat {
+    .grid-item:nth-child(#{length($colors)}n+#{$i}) {
+        background: lighten(nth($colors, random(length($colors))), 20%);
+    }
+}
+
+body {
+  margin: 0;
+}
+  .search {
+    color: #999;
+    display: inline-block;
+    font: inherit;
+    font-size: 4em;
+    height: 90px;
+    outline: none;
+    padding: 20px;
+    transition: border-color 500ms ease;
+    width: 100%;
+  }
+  .grid {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    flex-wrap: wrap;
+    .grid-item {
+      width: 350px;
+      flex: 1;
+      max-height: 350px;
+      min-height: 350px;
+    }
+  }
+</style>
